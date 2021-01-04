0b1f468b2be2900c4e83223b4621e9b8711f9245
ulzaman <ulzaman415@gmail.com>
2021-01-05 00:22:33 +0530

    Add project files.

---
+++ ZRQA/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZRQA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZRQA")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("705d146e-29b7-4805-8fbf-44fe299cbc7e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ZRQA/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Reflection;
+using System.IO;
+using System.Configuration;
+namespace ZRQA
+{
+    [TestClass]
+    public class LogIn
+    {
+        public static IWebDriver driver { get; set; }
+        [TestMethod]
+        public void ValidateLogin()
+
+        {
+            var chromOptions = new ChromeOptions();
+            chromOptions.AddArgument("-no-sandbox");
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromOptions);
+            driver.Navigate().GoToUrl("https://zrqa.qwikcoverage.com/Login/Login.aspx");
+             
+             
+        }
+    }
+}
